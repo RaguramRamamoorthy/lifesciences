40aaee2cc586e707e762fe8af32a23f5a1ad7735
Megha Rani Aroor <meghaaroor92@gmail.com>
2021-12-28 21:17:10 +0530

    added chap2

--- apps/chap2.py
+++ apps/chap2.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 def app():
     st.title("Introduction to Deep Learning")
+    st.write("One of the simplest models is a linear model y = Mx+b")
+    st.write("Multilayer perceptron can be visualized as a stack of layers, where each layer corresponds to a linear "
+             "transformation followed by a nonlinearity")
+    st.write("Two very important classes of models that are widely used in many different fields "
+             "are convolutional neural networks and recurrent neural networks.")
+
