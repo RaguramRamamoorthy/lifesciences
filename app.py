0e0840105d180f4ab7685eb9e1d0eaf227dc7209
ragu <rrrtechie@gmail.com>
2021-12-28 15:19:52 +0530

    multipage

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+from apps import chap1
+from multiapp import MultiApp
 
 st.title("LIFE SCIENCES")
 st.write("by RaguRam")
+
+
+app = MultiApp()
+
+st.markdown("""
+This app summarises the book LifeSciences
+""")
+
+# Add all your application here
+app.add_app("Chapter1", chap1.app)
+
+# The main app
+app.run()
