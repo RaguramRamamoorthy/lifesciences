8ac040158117936cd216e88fa6fa26ac96c4dbb0
ragu <rrrtechie@gmail.com>
2021-12-28 15:29:11 +0530

    changes to app

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 import requests
 
+from apps import chap1
+from multiapp import MultiApp
+
+
 st.title("LIFE SCIENCES")
 st.write("by RaguRam")
 
@@ -14,5 +18,12 @@ def load_lottieurl(url: str):
 
 
 lottie_lifesciences = load_lottieurl('https://assets1.lottiefiles.com/packages/lf20_vtuepexz.json')
-
 st_lottie(lottie_lifesciences, height=200)
+
+app = MultiApp()
+
+# Add all your application here
+app.add_app("chapter1", chap1.app)
+
+# The main app
+app.run()
