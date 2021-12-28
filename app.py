648596776fc478e27b67af45bdc9886fa5b6be51
Megha Rani Aroor <meghaaroor92@gmail.com>
2021-12-28 15:18:54 +0530

    lottie file

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
 
 st.title("LIFE SCIENCES")
 st.write("by RaguRam")
+
+def load_lottieurl(url: str):
+
+    r = requests.get(url)
+
+    if r.status_code != 200:
+
+        return None
+
+    return r.json()
+
+lottie_lifesciences = load_lottieurl('https://assets1.lottiefiles.com/packages/lf20_vtuepexz.json')
+
+st_lottie(lottie_lifesciences, height=200)
