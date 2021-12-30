6f63965a791071bf88b6b138a3a75cfc5c553173
ragu <rrrtechie@gmail.com>
2021-12-30 18:48:46 +0530

    molnet

--- apps/chap3.py
+++ apps/chap3.py
@@ -1,3 +1,35 @@
-import streamlit as st
+from deepchem.molnet import load_tox21
+import deepchem as dc
+import numpy as np
 
-st.write("hello")
+# Load the data.
+
+tox21_tasks, tox21_datasets, transformers = load_tox21()
+
+train_dataset, valid_dataset, test_dataset = tox21_datasets
+
+print(train_dataset.X.shape)
+print(train_dataset.X[0].shape)
+print(train_dataset.y[0])
+print(valid_dataset.X.shape)
+print(test_dataset.X.shape)
+
+print(np.shape(train_dataset.y))
+print(np.shape(valid_dataset.y))
+print(np.shape(test_dataset.y))
+
+print(train_dataset.w.shape)
+print(np.count_nonzero(train_dataset.w))
+print(np.count_nonzero(train_dataset.w == 0))
+
+model = dc.models.MultitaskClassifier(n_tasks=12,
+                                      n_features=1024,
+                                      layer_sizes=[1000])
+
+model.fit(train_dataset, nb_epoch=10)
+
+metric = dc.metrics.Metric(dc.metrics.roc_auc_score, np.mean)
+train_scores = model.evaluate(train_dataset, [metric], transformers)
+test_scores = model.evaluate(test_dataset, [metric], transformers)
+print(train_scores)
+print(test_scores)
