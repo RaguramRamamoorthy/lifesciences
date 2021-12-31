29d04fa598fd24ae941326a95c49147d9205cf70
Megha Rani Aroor <meghaaroor92@gmail.com>
2021-12-31 19:17:46 +0530

    chap4 added

---
+++ apps/chap4.py
@@ -0,0 +1,36 @@
+import deepchem as dc
+from rdkit import Chem
+
+tasks, datasets, transformers = dc.molnet.load_delaney(featurizer='GraphConv')
+train_dataset, valid_dataset, test_dataset = datasets
+
+# Create and train the model.
+
+model = dc.models.GraphConvModel(n_tasks=1, mode='regression', dropout=0.2)
+model.fit(train_dataset, nb_epoch=100)
+
+# Evaluate it.
+
+metric = dc.metrics.Metric(dc.metrics.pearson_r2_score)
+print("Training set score")
+print(model.evaluate(train_dataset, [metric], transformers))
+print("Test set score")
+print(model.evaluate(test_dataset, [metric], transformers))
+
+# Use it to predict the solubility of some molecules.
+
+smiles = ['COC(C)(C)CCCC(C)CC=CC(C)=CC(=O)OC(C)C',
+          'CCOC(=O)CC',
+          'CSc1nc(NC(C)C)nc(NC(C)C)n1',
+          'CC(C#C)N(C)C(=O)Nc1ccc(Cl)cc1',
+          'Cc1cc2ccccc2cc1C']
+
+
+mols = [Chem.MolFromSmiles(s) for s in smiles]
+featurizer = dc.feat.ConvMolFeaturizer()
+x = featurizer.featurize(mols)
+predicted_solubility = model.predict_on_batch(x)
+for m, s in zip(smiles, predicted_solubility):
+    print()
+    print('Molecule:', m)
+    print('Predicted solubility:', s)
